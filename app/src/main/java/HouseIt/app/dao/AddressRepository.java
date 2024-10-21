109ae36e71c0527d47d3cec9969d18be3104e5fd
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2024-10-21 10:56:26 -0400

    Update AddressRepository.java

--- app/src/main/java/HouseIt/app/dao/AddressRepository.java
+++ app/src/main/java/HouseIt/app/dao/AddressRepository.java
@@ -5,5 +5,15 @@ import HouseIt.app.model.Address;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AddressRepository extends CrudRepository<Address, Integer> {
-    
+
+    Address findAddressByCity(String city);
+
+    Address findAddressByStreet(String street);
+
+    Address findAddressByPostalCode(String postalCode);
+
+    Address findAddressByStreetNumber(int streetNumber);
+
+    Address findAddressByApartmentNumber(int apartmentNumber);
+
 }
