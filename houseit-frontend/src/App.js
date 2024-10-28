ffa98db3f272a308abef1ba99518c7275f5f7bb3
SamySabir <samysbr@hotmail.fr>
2024-10-28 00:13:21 -0400

    Added Image Upload component and added routing for listing in App.js

--- houseit-frontend/src/App.js
+++ houseit-frontend/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import SignUp from './components/sign-up/SignUp';
+import ImageUpload from './components/image-upload/ImageUpload';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+console.log(SignUp); // Should be a function
+console.log(ImageUpload); // Should be a function
 
 function App() {
   return (
-    <div>
-      <h1>HouseIt</h1>
-      <SignUp />
-    </div>
+    <Router>
+      <div>
+        <h1>HouseIt</h1>
+        
+        {/* Navigation Links */}
+        <nav>
+          <Link to="/listing">Create Listing</Link>
+        </nav>
+        
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/listing" element={<ImageUpload />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
