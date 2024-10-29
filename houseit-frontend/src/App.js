753fb36c88aa1b870e9fb7ef3cd6ce53276283c2
jkzcodes <jessiekurtz123@gmail.com>
2024-10-29 11:41:47 -0400

    Added a landing page that redirects to Vlad's component

--- houseit-frontend/src/App.js
+++ houseit-frontend/src/App.js
@@ -2,27 +2,35 @@ import './App.css';
 import SignUp from './components/sign-up/SignUp';
 import ImageUpload from './components/image-upload/ImageUpload';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import LandingPage from "./components/landing-page/LandingPage";
 console.log(SignUp); // Should be a function
 console.log(ImageUpload); // Should be a function
 
 function App() {
   return (
-    <Router>
-      <div>
-        <h1>HouseIt</h1>
-        
-        {/* Navigation Links */}
-        <nav>
-          <Link to="/listing">Create Listing</Link>
-        </nav>
-        
-        {/* Define Routes */}
-        <Routes>
-          <Route path="/" element={<SignUp />} />
-          <Route path="/listing" element={<ImageUpload />} />
-        </Routes>
-      </div>
-    </Router>
+
+      <Router>
+          <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/signup" element={<SignUp />} />
+          </Routes>
+      </Router>
+      // <Router>
+      //   <div>
+      //     <h1>HouseIt</h1>
+      //
+      //     {/* Navigation Links */}
+      //     <nav>
+      //       <Link to="/listing">Create Listing</Link>
+      //     </nav>
+      //
+      //     {/* Define Routes */}
+      //     <Routes>
+      //       <Route path="/" element={<SignUp />} />
+      //       <Route path="/listing" element={<ImageUpload />} />
+      //     </Routes>
+      //   </div>
+      // </Router>
   );
 }
 
