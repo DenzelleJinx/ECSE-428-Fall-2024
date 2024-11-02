c4a4b65ee73f6143457328968a73e267ec2ac735
abdelrahmanali <abdalrahman2587@gmail.com>
2024-11-02 15:43:24 -0400

    Updated controllers and services for Landlord and Student to update User info through Endpoints

--- HouseIt-Backend/src/main/java/HouseIt/controller/LandlordController.java
+++ HouseIt-Backend/src/main/java/HouseIt/controller/LandlordController.java
@@ -3,13 +3,16 @@ package HouseIt.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import HouseIt.dto.LandlordDTO;
 import HouseIt.model.Landlord;
+import HouseIt.model.User.AccountStatus;
 import HouseIt.service.LandlordService;
 
 
@@ -34,5 +37,20 @@ public class LandlordController {
         return ResponseEntity.created(null).body(dto);
     }
     
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<LandlordDTO> updateLandlord(
+        @PathVariable int id,
+        @RequestParam(required = false) String username,
+        @RequestParam(required = false) String password,
+        @RequestParam(required = false) String email,
+        @RequestParam(required = false) String phoneNumber,
+        @RequestParam(required = false) AccountStatus status,
+        @RequestParam(required = false) Float rating) {
+
+        Landlord updatedLandlord = landlordService.updateLandlord(id, username, password, email, phoneNumber, status, rating);
+        LandlordDTO updatedDto = landlordService.convertToDTO(updatedLandlord);
+
+        return ResponseEntity.ok(updatedDto);
+    }
     
 }
