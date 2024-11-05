711ee22de106b3f1202ac4d73d4ac5f23fd9eefd
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2024-11-04 21:21:33 -0500

    Implement controller method for updateListing

--- HouseIt-Backend/src/main/java/HouseIt/controller/ListingController.java
+++ HouseIt-Backend/src/main/java/HouseIt/controller/ListingController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import HouseIt.dto.ListingDTO;
 import HouseIt.model.Listing;
@@ -49,6 +51,31 @@ public class ListingController {
         Listing listing = listingService.createListing(title, description, monthlyPrice, propertyRating, bedrooms, bathrooms, propertyType, squareFootage, wheelchairAccessible, hidden, smokingAllowed, address, amenitiesOffered, null, utilitiesCosts);
         ListingDTO dto = listingService.convertToDTO(listing);
         return ResponseEntity.created(null).body(dto);
+    }
+
+    @PutMapping(value = {"", "/{id}"})
+    public ResponseEntity<ListingDTO> updateListing(
+        @PathVariable int id,
+        @RequestParam String title,
+        @RequestParam String description,
+        @RequestParam int monthlyPrice,
+        @RequestParam float propertyRating,
+        @RequestParam int bedrooms,
+        @RequestParam int bathrooms,
+        @RequestParam PropertyType propertyType,
+        @RequestParam int squareFootage,
+        @RequestParam boolean wheelchairAccessible,
+        @RequestParam boolean hidden,
+        @RequestParam boolean smokingAllowed,
+        @RequestParam Address address,
+        @RequestParam Amenities amenitiesOffered,
+        @RequestParam Utilities utilitiesCosts,
+        @RequestParam List<Image> propertyImages )
+         {
+
+        Listing listing = listingService.updateListing(id, title, description, monthlyPrice, propertyRating, bedrooms, bathrooms, propertyType, squareFootage, wheelchairAccessible, hidden, smokingAllowed, address, amenitiesOffered, null, utilitiesCosts);
+        ListingDTO dto = listingService.convertToDTO(listing);
+        return ResponseEntity.ok(dto);
     }
     
 }
